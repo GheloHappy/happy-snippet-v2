65f3d6f8b9d1e8790d7ac9d3fa62324ea3b24ee6
Ghelowee <ghelonicomaligaya111@gmail.com>
2025-04-01 21:23:32 +0800

    update: tailwind setup

---
+++ app/app/index.tsx
@@ -0,0 +1,13 @@
+import {Animated, Text} from "react-native";
+import View = Animated.View;
+import "../global.css"
+
+export default function Login() {
+    return (
+        <>
+            <View className="flex-1 items-center justify-center w-full">
+                <Text>Login</Text>
+            </View>
+        </>
+    )
+}
