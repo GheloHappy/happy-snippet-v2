ab188adc1839ba59f80162b404e01ce52b534e71
Ghelowee <ghelonicomaligaya111@gmail.com>
2025-10-04 13:33:19 +0800

    cleaning

--- api/routes/user-routes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {insertUser_Cont} from "../controllers/user-cont.js";
-
-const router = express.Router();
-const version: string = process.env.API_VERSION || 'v1';
-
-router.post(`/api/${version}/user`, insertUser_Cont);
-
-export default router;
--- api/utils/db/creds.ts
+++
@@ -1,4 +0,0 @@
-export const database: string | undefined = process.env.DB;
-export const user: string | undefined = process.env.DB_USER;
-export const password: string | undefined = process.env.DB_PASSWORD;
-export const server: string | undefined = process.env.DB_SERVER;
