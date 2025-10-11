3712448d9c4aa8b62b8be802a130fd28e3e4929a
Ghelowee <ghelonicomaligaya111@gmail.com>
2025-10-11 15:55:14 +0800

    update

--- web/src/pages/AuthPage.tsx
+++ web/src/pages/AuthPage.tsx
@@ -2,7 +2,7 @@
 export default function AuthPage() {
     return (
         <div className="min-h-screen 
-        flex justify-center items-center p-5 bg-default">
+        flex justify-center items-center p-5 bg-default"> 
             <h1 className="text-2xl font-bold text-red-50">Hello Snippet</h1>
         </div>
     )
