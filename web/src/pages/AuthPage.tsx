568a45e519f0a698ecd75026e0e051c3ee15d1ff
Ghelowee <ghelonicomaligaya111@gmail.com>
2025-10-12 16:15:53 +0800

    update: auth

--- web/src/pages/AuthPage.tsx
+++ web/src/pages/AuthPage.tsx
@@ -1,9 +1,16 @@
+import {
+    Skull
+} from 'lucide-react';
+
 
 export default function AuthPage() {
     return (
-        <div className="min-h-screen 
-        flex justify-center items-center p-5 bg-default">
-            <h1 className="text-2xl font-bold text-red-50">Hello Snippet</h1>
+        <div className="min-h-screen flex justify-center items-center p-5 bg-default">
+            <div className="flex flex-col w-full items-center justify-center max-w-5xl md:h-[50vh] md:overflow-hidden">
+                <Skull className="w-16 h-16 text-red-500 animate-pulse" />
+                <h1 className="text-2xl text-heading">Happy Snipet</h1>
+                <p className="text-gray-400">Sign in to access your snippets</p>
+            </div>
         </div>
     )
 }
